ed294719da79371c8d4fa9a765fbea6050ff9347
fabbitox <hyunjin_11@naver.com>
2023-04-10 13:38:23 +0900

    javascript start

---
+++ scripts/13.js
@@ -0,0 +1,8 @@
+function show() {
+    //alert("안녕하세요");
+    document.getElementById("h2id").innerHTML = "반갑습니다";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("h2id").innerHTML = "안녕하세요";
+});
