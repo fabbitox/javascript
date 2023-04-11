eaefe9a63d2f9f0a873b0450dd0d72c261b42bd5
fabbitox <hyunjin_11@naver.com>
2023-04-11 11:14:25 +0900

    DOM control & dice

--- scripts/13.js
+++ scripts/13.js
@@ -1,8 +1,24 @@
-function show() {
+/* function show() {
     //alert("안녕하세요");
-    document.getElementById("h2id").innerHTML = "반갑습니다";
-}
-
-document.addEventListener("DOMContentLoaded", function () {
+    event.preventDefault();
     document.getElementById("h2id").innerHTML = "안녕하세요";
+} */
+
+const show = () => {
+    event.preventDefault();
+    document.getElementById("h2id").innerHTML = '<img src="./img/' + Math.ceil(Math.random() * 6) + '.png">';
+};
+
+/* document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("h2id").innerHTML = "안녕하세요";
+}); */
+
+document.addEventListener("DOMContentLoaded", () => {
+    //document.getElementById("h2id").innerHTML = "시작입니다";
+    //document.querySelector("#h2id").innerHTML = "아이디 선택자";
+    //document.querySelector(".h2class").innerHTML = "클래스 선택자";
+    // create
+    /* const button = document.createElement('button');
+    button.textContent = "확인";
+    document.getElementById("hdiv").append(button); */
 });
