623eca6e308132e5db340877a6a3f563d02e4d0a
fabbitox <hyunjin_11@naver.com>
2023-04-12 11:12:32 +0900

    add my code

--- scripts/16.js
+++ scripts/16.js
@@ -4,21 +4,34 @@ const show = () => {
     //랜덤 수 생성
     let n = Math.floor(Math.random() * 6) + 1;
     
-    let user;
     const radios = document.querySelectorAll('input[type=radio]');
-    for (let item of radios) {
-        console.log(item);
-        if (item.checked) {
-            user = item.value;
+    // let user;
+    // for (let item of radios) {
+    //     console.log(item);
+    //     if (item.checked) {
+    //         user = item.value;
+    //         break;
+    //     }
+    // }
+
+    // if (n == user) {
+    //     document.querySelector('article h1').textContent = "축하합니다";
+    // } else {
+    //     document.querySelector('article h1').textContent = "";
+    // }
+
+    let i;
+    for (i = 0; i < n; i++) {
+        console.log(radios[i]);
+        if (radios[i].checked) {
             break;
         }
     }
-
-    document.querySelector(".h2Class").innerHTML = `<img src="./images/${n}.png">`;
-
-    if (n == user) {
+    if (i + 1 == n) {
         document.querySelector('article h1').textContent = "축하합니다";
     } else {
         document.querySelector('article h1').textContent = "";
     }
+
+    document.querySelector(".h2Class").innerHTML = `<img src="./images/${n}.png">`;
 }
