340f56edc1790fc33700f8054a00c5eb1f7f5f93
fabbitox <hyunjin_11@naver.com>
2023-04-12 12:50:16 +0900

    select change

--- scripts/17.js
+++ scripts/17.js
@@ -12,10 +12,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     fromsel.addEventListener("change", () => {
         fromtx.textContent = fromsel.value;
+        fromsel.value == '℃' ? tosel.value = '℉' : tosel.value = '℃';
         calculate();
     });
     tosel.addEventListener("change", () => {
         totx.textContent = tosel.value;
+        tosel.value == '℃' ? fromsel.value = '℉' : fromsel.value = '℃';
         calculate();
     });
 
@@ -25,12 +27,10 @@ document.addEventListener("DOMContentLoaded", () => {
         } else {
             toval.value = tosel.value == '℃' ? (fromval.value - 32) * 5 / 9 : fromval.value;
         } */
-        if (fromsel.value == tosel.value) {
+        /* if (fromsel.value == tosel.value) {// 같을 일이 없어짐
             toval.value = fromval.value;
-        }
-        else {
-            toval.value = tosel.value == '℃' ? fromval.value * 1.8 + 32 : (fromval.value - 32) * 5 / 9;
-        }
+        } */
+        toval.value = tosel.value == '℃' ? fromval.value * 1.8 + 32 : (fromval.value - 32) * 5 / 9;
     };
 
     fromval.addEventListener("input", calculate);
