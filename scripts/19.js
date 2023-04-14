53c92acd473b5a1f03b78a5d570ad80e825ea776
fabbitox <hyunjin_11@naver.com>
2023-04-14 11:01:34 +0900

    use function

--- scripts/19.js
+++ scripts/19.js
@@ -5,16 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const inputx = document.querySelector("#inputx");
     const rstbtn = document.querySelector("#rstbtn");
 
-    let arr = [];
+    let emojiArray = [];
     rstbtn.addEventListener("click", () => {
-        arr = [];
+        emojiArray = [];
     });
 
     for (let btn of btns) {
         btn.addEventListener("click", (e) => {
             e.preventDefault();
             console.log(btn.textContent);
-            switch (btn.textContent) {
+            emojiArray.push(getEmoji(btn.textContent));
+            /* switch (btn.textContent) {
                 case "딸기":
                     arr.push('🍓');
                     break;
@@ -27,29 +28,62 @@ document.addEventListener("DOMContentLoaded", () => {
                 case "바나나":
                     arr.push('🍌');
                     break;
-            }
-            console.log(arr);
-            inputx.value = arr.join('');
+            } */
+            console.log(emojiArray);
+            inputx.value = emojiArray.join('');
         });
     }
     for (let delbtn of delbtns) {
-        delbtn.addEventListener("click", () => {
+        delbtn.addEventListener("click", (e) => {
             e.preventDefault();
-            switch (btn.textContent) {
+            //const fruit = delbtn.textContent.replace(" 삭제", "");
+            const btnText = delbtn.textContent;
+            const fruit = btnText.substring(0, btnText.indexOf(' '));
+            emojiArray = emojiArray.filter((item) => item != getEmoji(fruit));
+            /* switch (fruit) {
                 case "딸기":
+                    arr = arr.filter((item) => item != '🍓');
                     break;
                 case "오렌지":
+                    arr = arr.filter((item) => item != '🍊');
                     break;
                 case "사과":
+                    arr = arr.filter((item) => item != '🍏');
                     break;
                 case "바나나":
+                    arr = arr.filter((item) => item != '🍌');
                     break;
-            }
+            } */
+            //arr = delItem(fruit, arr);
+            inputx.value = emojiArray.join('');
         });
     }
     for (let chngbtn of chngbtns) {
-        chngbtn.addEventListener("click", () => {
+        chngbtn.addEventListener("click", (e) => {
             e.preventDefault();
         });
     }
 });
+
+const delItem = (itemName, arr) => {
+    let result = [];
+    for (let item of arr) {
+        if (item != getEmoji(itemName)) {
+            result.push(item);
+        }
+    }
+    return result;
+}
+
+const getEmoji = (itemName) => {
+    switch (itemName) {
+        case "딸기":
+            return '🍓';
+        case "오렌지":
+            return '🍊';
+        case "사과":
+            return '🍏';
+        case "바나나":
+            return '🍌';
+    }
+}
