614cd5548a79b4c3c7b46b51916872a614048087
fabbitox <hyunjin_11@naver.com>
2023-04-21 12:38:03 +0900

    function

--- scripts/21.js
+++ scripts/21.js
@@ -1,6 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     // 어제 날짜
     let url = 'https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20230419';
+    const boxdiv = document.querySelector('#boxdiv');
+    updateBox(url, boxdiv);
+
+    const boxdate = document.querySelector('#boxdate');
+    boxdate.addEventListener('change', () => {
+        url = 'https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=';
+        url += boxdate.value.replaceAll('-', '');
+        updateBox(url, boxdiv);
+    });
+})
+
+const updateBox = (url, boxdiv) => {
     fetch(url).then((resp) =>
         resp.json()
     ).then((data) => {
@@ -16,33 +28,8 @@ document.addEventListener('DOMContentLoaded', () => {
             boxtag += '</details>';
             console.log(box.rank, box.movieNm, box.salesAmt, box.rankInten);
         }
-        document.querySelector('#boxdiv').innerHTML = boxtag;
+        boxdiv.innerHTML = boxtag;
     }).catch((err) => {
         console.log(err);
     });
-
-    const boxdate = document.querySelector('#boxdate');
-    boxdate.addEventListener('change', () => {
-        url = 'https://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=';
-        url += boxdate.value.replaceAll('-', '');
-        fetch(url).then((resp) =>
-            resp.json()
-        ).then((data) => {
-            let boxlist = data.boxOfficeResult.dailyBoxOfficeList;
-            let boxtag = '';
-            for (let box of boxlist) {
-                boxtag += '<details id=\'d';
-                boxtag += `${box.rank}'>`;
-                boxtag += `<summary>${box.movieNm}</summary>`;
-                boxtag += '<ul>'
-                boxtag += `<li>순위: ${box.rank}</li><li>매출액: &#8361;${parseInt(box.salesAmt).toLocaleString()}</li><li>증감: ${box.rankInten}</li>`;
-                boxtag += '</ul>'
-                boxtag += '</details>';
-                console.log(box.rank, box.movieNm, box.salesAmt, box.rankInten);
-            }
-            document.querySelector('#boxdiv').innerHTML = boxtag;
-        }).catch((err) => {
-            console.log(err);
-        });
-    });
-})
+}
